f5158eb2dda8c305e6df8d444f215119c12c1511
haribo0 <haleey8202@gmail.com>
2025-04-09 16:47:50 +0900

    feat: 9012번 괄호 (스택)

---
+++ challenges/baekjoon/silverIV/p9012/Main.java
@@ -0,0 +1,54 @@
+package challenges.baekjoon.silverIV.p9012;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    private static boolean isValidParentheses(String s) {
+        int balance = 0;
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                balance++;
+            } else if (ch == ')') {
+                balance--;
+            }
+
+            if (balance < 0) {
+                return false;
+            }
+        }
+
+        return balance == 0;
+    }
+    
+    private static boolean isValidVPS(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                stack.push(ch);
+            } else { // ch == ')'
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+
+        return stack.isEmpty();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = Integer.parseInt(sc.nextLine());
+
+        for (int t = 0; t < T; t++) {
+            String line = sc.nextLine();
+            if (isValidVPS(line)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
