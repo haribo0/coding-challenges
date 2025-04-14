055e18bb1c8c7c75e825a04d55b222e765ca8c5b
haribo0 <haleey8202@gmail.com>
2025-04-14 18:33:04 +0900

    feat: [b1918] 후위 표기식 (스택)

---
+++ challenges/baekjoon/goldII/p1918/Main.java
@@ -0,0 +1,44 @@
+package challenges.baekjoon.goldII.p1918;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    private static int precedence(char op) {
+        if (op == '+' || op == '-') return 1;
+        if (op == '*' || op == '/') return 2;
+        return 0; // '('일 때
+    }
+    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String infix = sc.nextLine();
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch : infix.toCharArray()){
+            if(Character.isLetter(ch)){ // 문자
+                sb.append(ch);
+            }else if(ch=='('){  // 괄호 열기
+                stack.push(ch);
+            }else if(ch==')'){  // 괄호 닫기 
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    sb.append(stack.pop());
+                }
+                stack.pop();
+            }else{  // 연산자
+                while (!stack.isEmpty() && precedence(stack.peek()) >= precedence(ch)) {
+                    sb.append(stack.pop());
+                }
+                stack.push(ch);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+
+        System.out.print(sb.toString());
+    }
+}
