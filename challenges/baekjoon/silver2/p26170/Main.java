0443f2ee37b753f021b75745a69f81dc4a90518c
haribo0 <haleey8202@gmail.com>
2025-06-27 16:46:25 +0900

    refactor: [b26170] 사과 빨리 먹기

--- challenges/baekjoon/silver2/p26170/Main.java
+++ challenges/baekjoon/silver2/p26170/Main.java
@@ -1,65 +1,76 @@
 package challenges.baekjoon.silver2.p26170;
-
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static final int N = 5;
-    static int[][] board = new int[N][N];
-    static boolean[][][][] visited = new boolean[N][N][4][1 << 25]; // 최악 대비하면 비트마스크는 따로 처리하는 게 더 좋음
+    static int[][] board = new int[5][5];
     static int[] dr = {-1, 1, 0, 0};
     static int[] dc = {0, 0, -1, 1};
 
     static class State {
-        int r, c, apple, steps;
-        boolean[][] block;
+        int r, c, apple, step;
+        int bitmask;
 
-        public State(int r, int c, int apple, int steps, boolean[][] block) {
+        State(int r, int c, int apple, int step, int bitmask) {
             this.r = r;
             this.c = c;
             this.apple = apple;
-            this.steps = steps;
-            this.block = new boolean[N][N];
-            for (int i = 0; i < N; i++)
-                this.block[i] = block[i].clone();
+            this.step = step;
+            this.bitmask = bitmask;
         }
     }
 
-    public static int bfs(int sr, int sc) {
-        Queue<State> q = new LinkedList<>();
-        boolean[][] initBlock = new boolean[N][N];
-        initBlock[sr][sc] = true;
-        q.offer(new State(sr, sc, 0, 0, initBlock));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        while (!q.isEmpty()) {
-            State cur = q.poll();
-            if (cur.apple == 3) return cur.steps;
+        for (int i = 0; i < 5; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 5; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int sr = Integer.parseInt(st.nextToken());
+        int sc = Integer.parseInt(st.nextToken());
+
+        System.out.println(bfs(sr, sc));
+    }
+
+    static int bfs(int sr, int sc) {
+        Queue<State> queue = new LinkedList<>();
+        Set<String> visited = new HashSet<>();
+
+        int startIdx = sr * 5 + sc;
+        int startBitmask = (1 << startIdx);
+        queue.add(new State(sr, sc, 0, 0, startBitmask));
+        visited.add(sr + "," + sc + ",0," + startBitmask);
+
+        while (!queue.isEmpty()) {
+            State cur = queue.poll();
+
+            if (cur.apple == 3) {
+                return cur.step;
+            }
 
             for (int d = 0; d < 4; d++) {
                 int nr = cur.r + dr[d];
                 int nc = cur.c + dc[d];
+                if (nr < 0 || nr >= 5 || nc < 0 || nc >= 5) continue;
 
-                if (nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
-                if (board[nr][nc] == -1 || cur.block[nr][nc]) continue;
+                int idx = nr * 5 + nc;
+                if (((cur.bitmask >> idx) & 1) == 1) continue;
+                if (board[nr][nc] == -1) continue;
 
-                int newApple = cur.apple + (board[nr][nc] == 1 ? 1 : 0);
-                boolean[][] newBlock = cur.block;
-                newBlock[cur.r][cur.c] = true;
+                int newApple = cur.apple;
+                if (board[nr][nc] == 1) newApple++;
 
-                q.offer(new State(nr, nc, newApple, cur.steps + 1, newBlock));
+                int newBitmask = cur.bitmask | (1 << (cur.r * 5 + cur.c));
+                String key = nr + "," + nc + "," + newApple + "," + newBitmask;
+                if (visited.contains(key)) continue;
+                visited.add(key);
+                queue.add(new State(nr, nc, newApple, cur.step + 1, newBitmask));
             }
         }
-
         return -1;
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < N; i++)
-            for (int j = 0; j < N; j++)
-                board[i][j] = sc.nextInt();
-        int sr = sc.nextInt();
-        int sc_ = sc.nextInt();
-
-        System.out.println(bfs(sr, sc_));
     }
 }
